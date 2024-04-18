06c0d53fda7be04149ad0c0a02e8e96e3dd58372
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2024-04-19 02:12:06 +0530

    frontend component done

---
+++ pages/index.tsx
@@ -0,0 +1,55 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import PrimaryButton from "../components/Button";
+import JoinRoomDialogue from "../components/joinRoomDialogue";
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col space-y-10 bg-gradient-to-b from-slate-800 via-bgpink to-bgdark min-h-screen text-white justify-center items-center">
+      <Head>
+        <title>CodeCollab</title>
+        <meta
+          name="description"
+          content="Code Online is an online community for testing and showcasing user-created HTML, CSS and JavaScript code snippets. It functions as an online code editor and open-source learning environment, where developers can create code snippets, "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+       <div className="flex justify-center items-center mx-5 space-x-8">
+        <div className=" flex flex-col justify-center items-center space-y-4 ">
+          <h1 className="font-extrabold text-5xl md:text-6xl text-center">
+            Code Here.{" "} 
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-textpink to-textblue">
+              Code Now.
+            </span>
+          </h1>
+
+          <div>
+            <JoinRoomDialogue />
+          </div>
+        </div>
+
+        <div className=" rounded-lg border-2 overflow-hidden">
+          <Image
+            src="/ww1.png"
+            width={800}
+            className="aspect-square"
+            height={500}
+          />
+        </div>
+      </div>
+       <footer className="fixed bottom-2">
+        Made with 💛 by{" "}
+        <a
+          className="text-primary ml-2 font-bold"
+          href="https://github.com/NayanPatil1998"
+        >
+          {" "}
+          Mangal Gupta
+        </a>
+      </footer>
+    </div>
+    );
+};
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        'editor': '280px 1fr',
+
+        // Complex site-specific column configuration
       },
+      colors: {
+        "primary" : "#4aee88",
+        "bgdark" : "#11082F",
+        "bgpink" : "#3A0D56",
+        "textpink": "#ED61E6",
+        "textblue": "#419AE5"
+
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
